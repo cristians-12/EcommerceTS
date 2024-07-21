68ba89a869acb7f69cfebdd0daa04c07a74e3a41
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-07-21 17:34:34 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["pbs.twimg.com"],
+  },
+};
+// next.config.js
 
 export default nextConfig;
