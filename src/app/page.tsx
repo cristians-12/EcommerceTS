2b90578c1bcdb810c8322127ef31ff227cfca564
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-07-21 17:50:31 -0500

    boton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [image, setImage] = useState<boolean>();
+
   return (
     <div>
-      <h1 className='text-[40px]'>Ecommerce</h1>
+      <h1 className="text-[40px]">Ecommerce</h1>
       <p>Si</p>
-      <Image alt='Hola' src='https://pbs.twimg.com/media/GS3WVdvbkAAifC3?format=jpg&name=4096x4096' width={800} height={300} />
+      <button onClick={() => setImage(!image)}>mostrar</button>
+      {image && (
+        <Image
+          alt="Hola"
+          src="https://pbs.twimg.com/media/GS3WVdvbkAAifC3?format=jpg&name=4096x4096"
+          width={800}
+          height={300}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
