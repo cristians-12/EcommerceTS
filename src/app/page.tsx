d38726adb90a3e2be2c5cbf42647bd475fdeaacd
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-07-21 17:58:18 -0500

    imagenes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,51 @@ const Home = () => {
       <p>Si</p>
       <button onClick={() => setImage(!image)}>mostrar</button>
       {image && (
-        <Image
-          alt="Hola"
-          src="https://pbs.twimg.com/media/GS3WVdvbkAAifC3?format=jpg&name=4096x4096"
-          width={800}
-          height={300}
-        />
+        <div className="flex flex-wrap gap-10">
+          <Image
+            alt="Hola"
+            src="https://pbs.twimg.com/media/GS3WVdvbkAAifC3?format=jpg&name=4096x4096"
+            width={400}
+            height={100}
+            quality={100}
+          />
+          <Image
+            alt="Hola"
+            src="https://pbs.twimg.com/media/GRXBlwEbUAAjxe6?format=jpg&name=4096x4096"
+            width={800}
+            height={300}
+          />
+          <Image
+            alt="Hola"
+            src="https://pbs.twimg.com/media/GDI4IrpbAAA2afi?format=jpg&name=medium"
+            width={800}
+            height={300}
+            quality={100}
+          />
+
+          <Image
+            alt="Hola"
+            src="https://pbs.twimg.com/media/GF3Z8WHaQAAc-hP?format=jpg&name=large"
+            width={800}
+            height={300}
+            quality={100}
+          />
+          <Image
+            alt="Hola"
+            src="https://pbs.twimg.com/media/F9fJ10NaMAArH4O?format=jpg&name=large"
+            width={800}
+            height={300}
+            quality={100}
+          />
+          
+          <Image
+            alt="Hola"
+            src="https://pbs.twimg.com/media/GBw2Z0KaEAAW1WH?format=jpg&name=medium"
+            width={800}
+            height={300}
+            quality={100}
+          />
+        </div>
       )}
     </div>
   );
