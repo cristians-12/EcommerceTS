be314c18e66c85d992ae8eb18ea13d6afb6440cb
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-07-21 18:05:49 -0500

    mejorando img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,25 @@ const Home = () => {
       <button onClick={() => setImage(!image)}>mostrar</button>
       {image && (
         <div className="flex flex-wrap gap-10">
+          <div className="w-[50vw]">
+            <Image
+              className="object-contain"
+              alt="Hola"
+              src="https://pbs.twimg.com/media/GS3WVdvbkAAifC3?format=jpg&name=4096x4096"
+              width={1000}
+              height={1000}
+              quality={100}
+            />
+          </div>
+          <div className="w-[40vw]">
           <Image
-            alt="Hola"
-            src="https://pbs.twimg.com/media/GS3WVdvbkAAifC3?format=jpg&name=4096x4096"
-            width={400}
-            height={100}
-            quality={100}
-          />
-          <Image
+          className="object-contain"
             alt="Hola"
             src="https://pbs.twimg.com/media/GRXBlwEbUAAjxe6?format=jpg&name=4096x4096"
-            width={800}
-            height={300}
+            width={1000}
+            height={1000}
           />
+          </div>
           <Image
             alt="Hola"
             src="https://pbs.twimg.com/media/GDI4IrpbAAA2afi?format=jpg&name=medium"
@@ -41,21 +47,26 @@ const Home = () => {
             height={300}
             quality={100}
           />
-          <Image
-            alt="Hola"
-            src="https://pbs.twimg.com/media/F9fJ10NaMAArH4O?format=jpg&name=large"
-            width={800}
-            height={300}
-            quality={100}
-          />
-          
-          <Image
-            alt="Hola"
-            src="https://pbs.twimg.com/media/GBw2Z0KaEAAW1WH?format=jpg&name=medium"
-            width={800}
-            height={300}
-            quality={100}
-          />
+          <div className="w-[50vw]">
+            <Image
+              className="object-cover"
+              alt="Hola"
+              src="https://pbs.twimg.com/media/F9fJ10NaMAArH4O?format=jpg&name=large"
+              width={1000}
+              height={300}
+              quality={100}
+            />
+          </div>
+
+          <div className="w-[50vw]">
+            <Image
+              alt="Hola"
+              src="https://pbs.twimg.com/media/GBw2Z0KaEAAW1WH?format=jpg&name=medium"
+              width={1000}
+              height={300}
+              quality={100}
+            />
+          </div>
         </div>
       )}
     </div>
