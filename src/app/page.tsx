338262aaa4304c7918595fbce6b5383c90f3d348
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-07-21 20:59:42 -0500

    suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
+import Loading from "./loading";
 
 const Home = () => {
   const [image, setImage] = useState<boolean>();
 
   return (
     <div>
-      <div className="flex bg-emerald-600 py-5 px-3">
-        <div className="bg-emerald-950 py-2 px-3 rounded-xl">
-          Todas las categorias
+      <Suspense fallback={<Loading />}>
+        <div className="flex bg-emerald-600 py-5 px-3">
+          <div className="bg-emerald-950 py-2 px-3 rounded-xl">
+            Todas las categorias
+          </div>
         </div>
-      </div>
+      </Suspense>
     </div>
   );
 };
